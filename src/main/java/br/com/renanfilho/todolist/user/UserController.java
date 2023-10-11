bebb75364fa61fce8055bc57fe0cceb870673191
Renan Filho <refilho@gmail.com>
2023-10-11 16:56:21 -0300

    Segunda Aula

--- src/main/java/br/com/renanfilho/todolist/user/UserController.java
+++ src/main/java/br/com/renanfilho/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.renanfilho.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired //Vai gerenciar o ciclo de vida do repositorio
+    private IUserRepository userRepository;
+
     /**
      * String (texto)
      * Integer (int) numeros inteiros
@@ -28,8 +34,17 @@ public class UserController {
      * Body
      */
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null) {
+            //Mensagem de erro
+            //Status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário ja existe");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
     
 }
--- src/main/java/br/com/renanfilho/todolist/user/UserModel.java
+++ src/main/java/br/com/renanfilho/todolist/user/UserModel.java
@@ -1,8 +1,38 @@
 package br.com.renanfilho.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name ="tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
     
+    //@Column(name = "cod_usuario") -Trocaria o nome da coluna no banco ao criar
+    @Column(unique = true)
     private String username;
+
     private String name;
+
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    //getters setters colocado via lombok pelo @Data
+
+    
 }
+
+
