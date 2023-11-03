09b95963eb0ae19265fd1e9d50c49561119587de
Renan Filho <refilho@gmail.com>
2023-11-03 17:48:56 -0300

    Aula 5 final

--- src/main/java/br/com/renanfilho/todolist/task/TaskController.java
+++ src/main/java/br/com/renanfilho/todolist/task/TaskController.java
@@ -56,13 +56,25 @@ public class TaskController {
 
     //http://localhost:8080/tasks/131321312 (id para alterar na rota)
     @PutMapping("/{id}")
-    public TaskModel update (@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request){
-
+    public ResponseEntity update (@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request){
         var task = this.taskRepository.findById(id).orElse(null);
+
+        if(task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Tarefa nao encontrada");
+        }
+
+        var idUser = request.getAttribute("idUser");
+
+        if (!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Usuario nao tem permissao para alterar esta tarefa.");
+        }
 
         Utils.copyNonNullProperties(taskModel, task);
 
-        return this.taskRepository.save(task);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
     }
     
 }
